fe3bde2f8fc2bce84d4b69003b02a15f869beeb6
Muhannad-09 <muhannadgsgs@gmail.com>
2025-08-19 22:54:12 +0000

    Iterating through report objects

---
+++ ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,12 @@
+export default function createIteratorObject(report) {
+  const allEmployees = report.allEmployees;
+  const employeesArray = [];
+
+  for (const department of Object.values(allEmployees)) {
+    for (const employee of department) {
+      employeesArray.push(employee);
+    }
+  }
+
+  return employeesArray[Symbol.iterator]();
+}
