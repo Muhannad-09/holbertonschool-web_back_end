f3e5f8d5e028628ca69f3d85c1aedf2217008e2e
Muhannad-09 <muhannadgsgs@gmail.com>
2025-08-19 23:00:01 +0000

    Iterating through report objects

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,5 @@
 export default function* createIteratorObject(report) {
-  const allEmployees = report.allEmployees;
-
-  for (const department of Object.values(allEmployees)) {
+  for (const department of Object.values(report.allEmployees)) {
     for (const employee of department) {
       yield employee;
     }
