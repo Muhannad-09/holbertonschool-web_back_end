5545a247588146ffdbdbea40be1b708796f9e08c
Muhannad-09 <muhannadgsgs@gmail.com>
2025-08-20 20:50:14 +0000

    You used to attend a place like this at some point

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,7 +1,5 @@
-  class ClassRoom {
-    constructor(maxStudentsSize) {
-      this._maxStudentsSize = maxStudentsSize;
-    }
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
   }
-
-  module.exports = ClassRoom;
+}
