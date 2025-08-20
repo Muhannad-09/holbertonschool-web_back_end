4618c32176a69b56d3eadddd00bed40456fe1e1e
Muhannad-09 <muhannadgsgs@gmail.com>
2025-08-20 20:55:15 +0000

    You used to attend a place like this at some point

---
+++ ES6_classes/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+};
