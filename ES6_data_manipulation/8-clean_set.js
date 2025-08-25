e0d8c4bda47d4cf324543ff79a48a0753904588b
Muhannad-09 <muhannadgsgs@gmail.com>
2025-08-25 12:51:21 +0000

    Clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,11 @@
 export default function cleanSet(set, startString) {
-  const result = [];
+  if (!startString) return '';
 
+  const result = [];
   for (const value of set) {
-    if (startString && value.startsWith(startString)) {
+    if (value.startsWith(startString)) {
       result.push(value.slice(startString.length));
-    } else if (!startString) {
-      result.push(value);
     }
   }
-
   return result.join('-');
 }
