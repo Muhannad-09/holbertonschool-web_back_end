40abc799e071ef14c930560a77cd602714f43393
Muhannad-09 <muhannadgsgs@gmail.com>
2025-09-01 12:55:59 +0000

    small HTTP server using Express

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
+// Catch-all route for unknown endpoints (return plain text)
+app.use((req, res) => {
+  res.status(404).send('Cannot GET ' + req.path);
+});
+
 app.listen(1245, () => {
   // eslint-disable-next-line no-console
   console.log('Express server running on port 1245');
