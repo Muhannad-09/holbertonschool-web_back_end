008e0d8dcb90242d17d009b2bb15f22af6987506
Muhannad-09 <muhannadgsgs@gmail.com>
2025-09-01 13:01:56 +0000

    Create a more complex HTTP server using Express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
-const databaseFile = process.argv[2]; // database.csv passed as argument
+const databaseFile = process.argv[2];
 
 const app = express();
 
@@ -9,13 +9,22 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  res.write('This is the list of our students\n');
-  countStudents(databaseFile)
-    .then(() => res.end())
-    .catch((err) => {
-      res.end(err.message);
-    });
+app.get('/students', async (req, res) => {
+  try {
+    // Capture console.log output from countStudents
+    let output = 'This is the list of our students\n';
+    const originalConsoleLog = console.log;
+    const logs = [];
+    console.log = (msg) => logs.push(msg);
+
+    await countStudents(databaseFile);
+
+    console.log = originalConsoleLog; // restore console.log
+    output += logs.join('\n');
+    res.send(output);
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 // Catch-all route for unknown endpoints (plain text)
