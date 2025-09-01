f8bc1afedf0b67bd633be93d19c579ec2c6c309c
Muhannad-09 <muhannadgsgs@gmail.com>
2025-09-01 12:48:57 +0000

    Create a small HTTP server using Node's HTTP module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server running on port 1245');
+});
+
+module.exports = app;
