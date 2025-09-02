d105bc8ddef00ee7f54b9604d0f1894672469fff
Muhannad-09 <muhannadgsgs@gmail.com>
2025-09-02 12:16:54 +0000

    Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,15 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
-  process.exit();
-});
-
-process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
+console.log('Welcome to Holberton School, what is your name?');
+// await input
+process.stdin.resume();
+// once input is entered
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+  // check if input is coming from the terminal
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
 });
