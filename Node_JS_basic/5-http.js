fd743cd4d9299a7e6fdd7f7e54b8fa18e0b9c1b0
Muhannad-09 <muhannadgsgs@gmail.com>
2025-09-02 12:56:22 +0000

    complex HTTP server using Node's HTTP

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,23 +3,25 @@ const countStudents = require('./3-read_file_async');
 
 const port = 1245;
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    const db = process.argv[2];
-    countStudents(db)
-      .then(() => res.end())
-      .catch((err) => {
-        res.end(err.toString());
-      });
-  } else {
-    res.end('Hello Holberton School!');
+const app = http.createServer(async (req, res) => {
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      res.end('Hello Holberton School!');
+    } else if (req.url === '/students') {
+      await countStudents(process.argv[2])
+        .then((val) => {
+          res.write('This is the list of our students\n');
+          res.write(`Number of students: ${val.arr.length}\n`);
+          res.write(`Number of students in CS: ${val.locateCS.length}. List: ${val.locateCS.join(', ')}\n`);
+          res.write(`Number of students in SWE: ${val.locateSWE.length}. List: ${val.locateSWE.join(', ')}\n`);
+          res.end();
+        })
+        .catch((err) => {
+          res.write('This is the list of our students\n');
+          res.end(err.message);
+        });
+    }
   }
 });
-
 app.listen(port);
-
 module.exports = app;
