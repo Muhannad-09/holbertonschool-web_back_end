11644f4221bcede5a92c44e88d7e36319fa2be70
Muhannad-09 <muhannadgsgs@gmail.com>
2025-09-02 13:13:29 +0000

    more complex HTTP server using Express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -18,14 +18,11 @@ app.get('/students', async (req, res) => {
   }
 });
 
-// Catch-all route for unknown endpoints (plain text)
 app.use((req, res) => {
   res.status(404).send('Cannot GET ' + req.path);
 });
 
 app.listen(1245, () => {
-  // eslint-disable-next-line no-console
   console.log('Express server running on port 1245');
 });
-
 module.exports = app;
