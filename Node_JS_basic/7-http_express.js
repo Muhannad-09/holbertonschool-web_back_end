e10a342a69cd92f945b4f5e85577d728e0cf29f4
Muhannad-09 <muhannadgsgs@gmail.com>
2025-09-07 11:17:22 +0000

    more complex HTTP server using Express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,26 +1,51 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const app = express();
-const port = 1245;
+const DB_FILE = process.argv[2];
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+    const lines = data.trim().split('\n');
+    const studentLines = lines.slice(1);
+    const validStudents = studentLines.filter((line) => line.trim() !== '');
+
+    let responseText = `Number of students: ${validStudents.length}`;
+    const fields = {};
+    for (const student of validStudents) {
+      const [firstname, , , field] = student.split(',');
+      if (field) {
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstname);
+      }
+    }
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        responseText += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+      }
+    }
+    resolve(responseText);
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  await countStudents(process.argv[2])
-    .then((val) => {
-      res.write('This is the list of our students\n');
-      res.write(`Number of students: ${val.arr.length}\n`);
-      res.write(`Number of students in CS: ${val.locateCS.length}. List: ${val.locateCS.join(', ')}\n`);
-      res.write(`Number of students in SWE: ${val.locateSWE.length}. List: ${val.locateSWE.join(', ')}\n`);
-      res.end();
+app.get('/students', (req, res) => {
+  countStudents(DB_FILE)
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
     })
     .catch((err) => {
-      res.write('This is the list of our students\n');
-      res.end(err.message);
+      res.send(`This is the list of our students\n${err.message}`);
     });
 });
-app.listen(port);
+
+app.listen(1245);
+
 module.exports = app;
